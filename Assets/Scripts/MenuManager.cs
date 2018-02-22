15a4e19cee31d6b01298757213b367bc64f2a13d
Dolev Pearl <19dolevp@gmail.com>
2018-02-22 14:05:45 +0000

    Main Menu & Music

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	void Start(){
+
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+	public void StartGame(){
+
+
+		SceneManager.LoadScene ("Main");
+
+	}
+
+}
