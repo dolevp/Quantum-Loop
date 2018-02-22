a347eddca3e40221eb3b8cbc2a7f9a87809a8bb9
Dolev Pearl <19dolevp@gmail.com>
2018-02-22 14:21:42 +0000

    Starting Panel

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour {
+
+
+	public Animator startAnim;
+
+	void Update(){
+
+
+		if (Input.GetMouseButton (0))
+			startAnim.Play ("Start");
+
+	}
+	
+
+}
